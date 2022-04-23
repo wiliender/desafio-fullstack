8670cd89da2f3453227398ad6a5e23ded0044a9d
Wiliender <wiliendersilva@gmail.com>
2022-04-23 14:28:29 -0300

    Criação do dispositivos Service, implementação dos botões salvar e cancelar e seus event binding

--- frontend/src/app/components/dispositivos/dispositivos-create/dispositivos-create.component.ts
+++ frontend/src/app/components/dispositivos/dispositivos-create/dispositivos-create.component.ts
@@ -1,4 +1,6 @@
+import { DispositivosService } from './../dispositivos.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dispositivos-create',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DispositivosCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dispositivosService: DispositivosService, private router: Router) { }
 
   ngOnInit(): void {
+    
   }
 
+  createDispositivos(): void {
+    this.dispositivosService.showMessage('Dispositivo criado com sucesso!')
+  }
+  cancel(): void {
+    this.router.navigate(['/dispositivos'])
+  }
 }
