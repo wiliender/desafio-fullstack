e7e397bfb37d5459251dce9b860f1eaf947f12b8
Wiliender <wiliendersilva@gmail.com>
2022-04-23 12:33:30 -0300

    update inserindo os novos paths categorias/create e dispositivos/create

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component'
 import { CategoriasCrudComponent } from './views/categorias-crud/categorias-crud.component'
 import { DispositivosCrudComponent } from './views/dispositivos-crud/dispositivos-crud.component'
+import { CategoriasCreateComponent } from './components/categorias/categorias-create/categorias-create.component'
+import { DispositivosCreateComponent } from './components/dispositivos/dispositivos-create/dispositivos-create.component';
+
 
 const routes: Routes = [
   {
@@ -14,6 +17,12 @@ const routes: Routes = [
   },{
     path: 'dispositivos',
     component: DispositivosCrudComponent
+  },{
+    path: 'categorias/create',
+    component: CategoriasCreateComponent
+  },{
+    path: 'dispositivos/create',
+    component: DispositivosCreateComponent
   }
 ];
 
