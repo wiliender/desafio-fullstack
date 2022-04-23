ea477b73c972124c02f659ceba6485904c4db314
Wiliender <wiliendersilva@gmail.com>
2022-04-23 11:41:33 -0300

    Criação do component DispositivosCrudComponent

---
+++ frontend/src/app/views/dispositivos-crud/dispositivos-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dispositivos-crud',
+  templateUrl: './dispositivos-crud.component.html',
+  styleUrls: ['./dispositivos-crud.component.css']
+})
+export class DispositivosCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
