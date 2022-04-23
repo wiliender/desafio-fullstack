6d014ad9d6154c4221b6296b945f3dd7b70dcbd3
Wiliender <wiliendersilva@gmail.com>
2022-04-23 12:31:24 -0300

    Criação dos components categorias-create e dispositivos-create

---
+++ frontend/src/app/components/categorias/categorias-create/categorias-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-create',
+  templateUrl: './categorias-create.component.html',
+  styleUrls: ['./categorias-create.component.css']
+})
+export class CategoriasCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/components/dispositivos/dispositivos-create/dispositivos-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dispositivos-create',
+  templateUrl: './dispositivos-create.component.html',
+  styleUrls: ['./dispositivos-create.component.css']
+})
+export class DispositivosCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
