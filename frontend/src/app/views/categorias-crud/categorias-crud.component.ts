291e0e0669e119405fb6de68ecd1ad36607389a9
Wiliender <wiliendersilva@gmail.com>
2022-04-23 11:40:41 -0300

    Criação do component CategoriasCrudComponent

---
+++ frontend/src/app/views/categorias-crud/categorias-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-crud',
+  templateUrl: './categorias-crud.component.html',
+  styleUrls: ['./categorias-crud.component.css']
+})
+export class CategoriasCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
