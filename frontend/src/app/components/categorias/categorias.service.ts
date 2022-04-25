2d3202733d14e941ca74a4dc9050e2de5a937674
Wiliender <wiliendersilva@gmail.com>
2022-04-24 21:13:46 -0300

    Criação da funcionalidade create e read

--- frontend/src/app/components/categorias/categorias.service.ts
+++ frontend/src/app/components/categorias/categorias.service.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { Categorias } from './categorias.model'
+
+//const url = 'http://localhost:3000/categorias';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  url = 'http://localhost:3000/categorias'
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
@@ -15,4 +22,13 @@ export class CategoriasService {
       verticalPosition: 'top',
     })
   }
+
+  create(categorias: Categorias): Observable<Categorias> {
+    return this.http.post<Categorias>(this.url, categorias)
+  }
+
+  read(): Observable<Categorias[]> {
+    return this.http.get<Categorias[]>(this.url)
+  }
 }
+
