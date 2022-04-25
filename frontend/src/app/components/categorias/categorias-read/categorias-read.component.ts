b6eb9438f19fcaed5683f3950b28283323cb73b1
Wiliender <wiliendersilva@gmail.com>
2022-04-24 21:21:42 -0300

    Feat: Criação da funcionalidade de Read, que faz uma requisição http do tipo GET para o backend

---
+++ frontend/src/app/components/categorias/categorias-read/categorias-read.component.ts
@@ -0,0 +1,24 @@
+import { CategoriasService } from './../categorias.service';
+import { Component, OnInit } from '@angular/core';
+import { Categorias } from '../categorias.model';
+
+@Component({
+  selector: 'app-categorias-read',
+  templateUrl: './categorias-read.component.html',
+  styleUrls: ['./categorias-read.component.css']
+})
+export class CategoriasReadComponent implements OnInit {
+
+  categorias!: Categorias[];
+  displayedColumns: string[] = ['id', 'name']
+
+  constructor(private categoriasService: CategoriasService) { }
+
+  ngOnInit(): void {
+    this.categoriasService.read().subscribe(categorias => {
+      this.categorias = categorias
+      console.log(categorias)
+    })
+  }
+
+}
