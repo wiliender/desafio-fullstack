77dde1e08ce77c11aaa50efa0276b494a05e5f06
Wiliender <wiliendersilva@gmail.com>
2022-04-29 23:44:46 -0300

    Upadate, criação do meotodo dispositivos create, associação do dispositivos ao categorias

--- frontend/src/app/components/dispositivos/dispositivos-create/dispositivos-create.component.ts
+++ frontend/src/app/components/dispositivos/dispositivos-create/dispositivos-create.component.ts
@@ -1,6 +1,8 @@
-import { DispositivosService } from './../dispositivos.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { CategoriasService } from './../../categorias/categorias.service';
+import { DispositivosService } from './../dispositivos.service';
 
 @Component({
   selector: 'app-dispositivos-create',
@@ -9,16 +11,34 @@ import { Router } from '@angular/router';
 })
 export class DispositivosCreateComponent implements OnInit {
 
-  constructor(private dispositivosService: DispositivosService, private router: Router) { }
+  dispositivos: any = {
+    categorias: {
+    },
+    color: '',
+    partNumber: '',
+  }
+  categorias: any;
+  constructor(private dispositivosService: DispositivosService, private router: Router, private categoriasService: CategoriasService) { }
 
   ngOnInit(): void {
-    
+    this.categoriasService.read().subscribe(categorias => {
+      this.categorias = categorias
+      console.log(categorias)
+    })
   }
-
+  
   createDispositivos(): void {
-    this.dispositivosService.showMessage('Dispositivo criado com sucesso!')
+    this.dispositivosService.create(this.dispositivos).subscribe(() => {
+      this.dispositivosService.showMessage('Dispositivo criado com sucesso!')
+      this.router.navigate(['/dispositivos'])
+    })
+    console.log(this.dispositivos)
   }
   cancel(): void {
     this.router.navigate(['/dispositivos'])
+  }
+  adicionarCateogira(categoria: any): void {
+    this.dispositivos.categorias = categoria
+    //this.categoriasService.update(categoria).subscribe()
   }
 }
