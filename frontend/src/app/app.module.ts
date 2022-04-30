7decd21ee3049c44a67f5fb076981e4cd90f5ef2
Wiliender <wiliendersilva@gmail.com>
2022-04-29 23:30:19 -0300

    Upadate

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,37 +1,38 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { CategoriasCreateComponent } from './components/categorias/categorias-create/categorias-create.component';
+import { CategoriasDeleteComponent } from './components/categorias/categorias-delete/categorias-delete.component';
+import { CategoriasReadComponent } from './components/categorias/categorias-read/categorias-read.component';
+import { CateogirasUpdateComponent } from './components/categorias/cateogiras-update/cateogiras-update.component';
+import { DispositivosCreateComponent } from './components/dispositivos/dispositivos-create/dispositivos-create.component';
+import { DispositivosReadComponent } from './components/dispositivos/dispositivos-read/dispositivos-read.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './views/home/home.component';
+import { RedDirective } from './directives/red.directive';
 import { CategoriasCrudComponent } from './views/categorias-crud/categorias-crud.component';
 import { DispositivosCrudComponent } from './views/dispositivos-crud/dispositivos-crud.component';
-import { RedDirective } from './directives/red.directive';
-import { CategoriasCreateComponent } from './components/categorias/categorias-create/categorias-create.component';
-import { DispositivosCreateComponent } from './components/dispositivos/dispositivos-create/dispositivos-create.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { HttpClientModule } from '@angular/common/http';
-
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { CategoriasReadComponent } from './components/categorias/categorias-read/categorias-read.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { CateogirasUpdateComponent } from './components/categorias/cateogiras-update/cateogiras-update.component';
-import { CategoriasDeleteComponent } from './components/categorias/categorias-delete/categorias-delete.component';
+import { HomeComponent } from './views/home/home.component';
+import { DispositivosDeleteComponent } from './components/dispositivos/dispositivos-delete/dispositivos-delete.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,9 @@ import { CategoriasDeleteComponent } from './components/categorias/categorias-de
     DispositivosCreateComponent,
     CategoriasReadComponent,
     CateogirasUpdateComponent,
-    CategoriasDeleteComponent
+    CategoriasDeleteComponent,
+    DispositivosReadComponent,
+    DispositivosDeleteComponent,
   ],
   imports: [
     BrowserModule,
@@ -65,7 +68,9 @@ import { CategoriasDeleteComponent } from './components/categorias/categorias-de
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
